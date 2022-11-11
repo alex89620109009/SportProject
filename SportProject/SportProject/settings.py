3d0141a5f83827a5626bd8cc0ba9e2873263c284
alex89620109009 <alex89620109009@gmail.com>
2022-11-11 04:43:38 +0300

    [#122143980] My commit message here.

--- SportProject/SportProject/settings.py
+++ SportProject/SportProject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'SportProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'project',
+        'USER': 'root',
+        'PASSWORD': 'Admin_11',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
